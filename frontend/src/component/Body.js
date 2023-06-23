b32cf19c9121fa1dd212bb57578c91a579fe2f3d
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2023-06-23 14:10:28 +0530

    feat: app working on local!🚀

--- frontend/src/component/Body.js
+++ frontend/src/component/Body.js
@@ -1,12 +1,21 @@
+import { useState } from "react";
+import axios from "axios";
+
 const Body = () => {
-    const onSubmitHandler = e => {
+    const [name, setName] = useState('')
+
+    const onSubmitHandler = async(e) => {
         e.preventDefault();
-        
+        const res = await axios.post('http://localhost:3000/api/v1/sending-digest',{name})
+        setName('');
+    }
+    const onChangeHandler = e => {
+        setName(e.target.value)
     }
     return (
 
         <form onSubmit={onSubmitHandler}>
-            <input placeholder="Enter notification text" type="text" />
+            <input placeholder="Enter notification text" type="text" onChange={onChangeHandler} value={name} />
             <button > Send </button>
         </form>
     )
--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -11,14 +11,14 @@ const Header = () => {
             window.location.href = message.cta.data.url;
         }
     }
-
     return (
-        <NovuProvider subscriberId={'digestSub'} applicationIdentifier={process.env.YOUR_APP_ID_FROM_ADMIN_PANEL}>
+        <NovuProvider subscriberId={'digestSub'} applicationIdentifier={process.env.REACT_APP_YOUR_APP_ID_FROM_ADMIN_PANEL}>
             <PopoverNotificationCenter onNotificationClick={onNotificationClick}>
-                {({ unseenCount }) => <NotificationBell unseenCount={unseenCount} />}
+                {({ unseenCount }) =>
+                    <NotificationBell unseenCount={unseenCount} />}
             </PopoverNotificationCenter>
         </NovuProvider>
     );
-}
+};
 
 export default Header
